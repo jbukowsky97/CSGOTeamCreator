6a072869ba310d93bac14be5523f2098de97f35c
Wesley <wesleyeguthrie@gmail.com>
2017-12-03 16:05:07 -0500

    Added login activity

--- app/src/main/java/com/hardboiled/csgoteamcreator/LoginActivity.java
+++ app/src/main/java/com/hardboiled/csgoteamcreator/LoginActivity.java
@@ -1,11 +1,24 @@
 package com.hardboiled.csgoteamcreator;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -14,16 +27,60 @@ public class LoginActivity extends AppCompatActivity {
     private EditText email;
     private EditText password;
 
+    private FirebaseAuth firebaseAuth;
+    private DatabaseReference databaseReference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        email = (EditText) findViewById(R.id.value_email);
+        password = (EditText) findViewById(R.id.password_text);
 
         Button login = (Button) this.findViewById(R.id.login_button);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent homeIntent = new Intent(LoginActivity.this, HomeActivity.class);
+                firebaseAuth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString()).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (!task.isSuccessful()) {
+                            Snackbar.make(findViewById(R.id.activity_sign_up_id), "Invalid credentials, please ensure the account exists.",
+                                    Snackbar.LENGTH_SHORT)
+                                    .show();
+                            return;
+                        }
+                    }
+                });
+                final Intent homeIntent = new Intent(LoginActivity.this, HomeActivity.class);
+                databaseReference = FirebaseDatabase.getInstance().getReference();
+                String uid = firebaseAuth.getCurrentUser().getUid();
+                Query query = databaseReference.child("users").orderByChild("uid").equalTo(uid);
+                query.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.exists()) {
+                            for (DataSnapshot value : dataSnapshot.getChildren()) {
+                                homeIntent.putExtra(value.getKey(), value.getValue().toString());
+                            }
+                        } else {
+                            Snackbar.make(findViewById(R.id.activity_sign_up_id), "Unable to find user in database",
+                                    Snackbar.LENGTH_SHORT)
+                                    .show();
+                            return;
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        Snackbar.make(findViewById(R.id.activity_sign_up_id), "Operation cancelled.",
+                                Snackbar.LENGTH_SHORT)
+                                .show();
+                        return;
+                    }
+                });
                 startActivity(homeIntent);
             }
         });
@@ -36,8 +93,5 @@ public class LoginActivity extends AppCompatActivity {
                 startActivityForResult(signupIntent, SIGN_UP_RESULT);
             }
         });
-
-        email = (EditText) findViewById(R.id.value_email);
-        password = (EditText) findViewById(R.id.password_text);
     }
 }
